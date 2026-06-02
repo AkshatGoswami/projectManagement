9a3d4befb83da64fc4e0a302dadacf90d0dece09
Akshat Goswami <91380683+AkshatGoswami@users.noreply.github.com>
2026-06-02 23:50:39 +0530

    Add logging for user ID during access token generation

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -75,6 +75,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 // Generating the Access token 
 userSchema.methods.generateAccessToken = function(){
     console.log("generating access token");
+    console.log("user id in access token generation", this._id);
     return jwt.sign(
         {
             _id: this._id,
