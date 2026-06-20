fa243fb78effdba19ff326e3c3806499656824c8
Akshat Goswami <91380683+AkshatGoswami@users.noreply.github.com>
2026-06-21 00:22:19 +0530

    Add models for notes, subtasks, tasks, and project members with schema definitions

--- src/models/projectMember.models.js
+++ src/models/projectMember.models.js
@@ -11,8 +11,11 @@ const projectMemberSchema = new Schema({
         type:Schema.Types.ObjectId,
         ref:"Project",
         required:true
-    }
+    },
     role:{
-        type
+        type:String,
+        enum:AvailableUserRole,
+        default:UserRolesEnum.MEMBER
     }
 },{timestamps:true})
+export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,1 +1,39 @@
 import mongoose, { Schema } from 'mongoose';
+
+const taskSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String
+    },
+    project:{
+        type:Schema.Types.ObjectId,
+        ref:"Project",
+        required:true
+    },
+    assignedTo:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    assignedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    status:{
+        type:String,
+        enum:AvailableTaskStatus,
+        default:TaskStatusEnum.TODO
+    },
+    attachments:{
+        type: [{
+            url: String,
+            mimetype: String,
+            size: Number,
+        }],
+        default: []
+    },
+},{timestamps:true})
+export const Task = mongoose.model("Task", taskSchema);
