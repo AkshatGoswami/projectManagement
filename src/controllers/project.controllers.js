46f206fe54b1e7881001d391746582099b2e4e98
Akshat Goswami <91380683+AkshatGoswami@users.noreply.github.com>
2026-06-22 00:53:39 +0530

    Add create project functionality with project member assignment

---
+++ src/controllers/project.controllers.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { Project } from "../models/project.models.js";
+
+const createProject = asyncHandler(async (req, res) => {
+    const { name, description } = req.body;
+    const project = await Project.create({
+        name,
+        description,
+        createdBy: new mongoose.Types.ObjectId(req.user._id)
+    })
+    if (!project) {
+        throw new ApiError(500, "Something went wrong while creating the project");
+    }
+    await projectMemeber.create({
+        user: new mongoose.Types.ObjectId(req.user._id),
+        project: new mongoose.Types.ObjectId(project._id),
+        role: UserRoles.ADMIN
+    })
+    return res.status(200).json(new ApiResponse(200, { project }, "Project created successfully"))
+})
