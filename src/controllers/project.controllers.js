a395f510c5c71377da4e332ddbe14ce5d97937f8
Akshat Goswami <91380683+AkshatGoswami@users.noreply.github.com>
2026-07-29 23:52:35 +0530

    Add get project functionality with aggregation and role retrieval

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -4,6 +4,8 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { Project } from "../models/project.models.js";
+import { ProjectMember } from "../models/projectMember.models.js";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const createProject = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
@@ -15,10 +17,10 @@ const createProject = asyncHandler(async (req, res) => {
     if (!project) {
         throw new ApiError(500, "Something went wrong while creating the project");
     }
-    await projectMemeber.create({
+    await ProjectMember.create({
         user: new mongoose.Types.ObjectId(req.user._id),
         project: new mongoose.Types.ObjectId(project._id),
-        role: UserRoles.ADMIN
+        role: UserRolesEnum.ADMIN
     })
     return res.status(200).json(new ApiResponse(200, { project }, "Project created successfully"))
 })
@@ -44,3 +46,68 @@ const deleteProject = asyncHandler(async (req, res) => {
     }
     return res.status(200).json(new ApiResponse(200, { project }, "Project deleted successfully"));
 });
+const getProject = asyncHandler(async (req, res) => {
+    const projects = await ProjectMember.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(req.user._id),
+            }
+        },
+        {
+            $lookup: {
+                from: "projects",
+                localField: "project",
+                foreignField: "_id",
+                as: "project",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "projectmembers",
+                            localField: "_id",
+                            foreignField: "project",
+                            as: "projectMembers",
+                        }
+                    },
+                    {
+                        $addFields: {
+                            members: {
+                                $size: "$projectMembers",
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: {
+                path: "$project",
+                preserveNullAndEmptyArrays: false
+            }
+        },
+        {
+            $project: {
+                project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy: 1,
+                },
+                role: 1,
+                _id: 0
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200,
+        projects,
+        "Project fetched successfully"
+    ))
+});
+
+export { 
+    createProject, 
+    updateProject, 
+    deleteProject,
+    getProject
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const UserRolesEnum = {
-    ADMIN = "admin",
-    PROJECT_ADMIN = "project_admin",
-    MEMBER = "member" 
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member" 
 }
 
 export const AvailableUserRoles = Object.values(UserRolesEnum)
